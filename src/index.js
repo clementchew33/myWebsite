716c83a7d2ed2509dfd519882117ef7de002bc9a
clementchew33 <clementchew33@gmail.com>
2026-04-03 17:46:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,8 +68,12 @@ export default {
 
     const discordResponse = await fetch(env.DISCORD_WEBHOOK_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: message })
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: message
+      })
     });
 
     if (!discordResponse.ok) {
